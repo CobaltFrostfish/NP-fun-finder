d9beac31e4a0d4e24ba6462b312476a9a65908e1
Evan Mora <evanrmora@gmail.com>
2021-03-30 20:01:50 -0600

    created the map, added rough stylings, updated sources in html

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,11 @@
 //api key= AIzaSyDFJN8KsV0vonVDnuMQg39pedzZxhh7rVs
 //above is for geolocation api https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
 
+let map;
 
-  
-
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'),{
+    center: { lat: -34.397, lng: 150.644},
+    zoom:8,
+  });
+}
