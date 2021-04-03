bf8836a0fc36524c44616abab6684b33db0531f4
Jared Neisen <Jared.Neisen@gmail.com>
2021-04-03 11:50:13 -0600

    it said i had some changes i guess

--- assets/script.js
+++ assets/script.js
@@ -32,23 +32,9 @@ function loadScript() {
   var script = document.createElement('script');                           //Create <script> element
   script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyAW220mOd1otBnP8lwhTJXTxRzMby1M2PE&callback=init';
 
-<<<<<<< HEAD
   var mapDiv = document.getElementById("mapDiv");
   mapDiv.appendChild(script); //add element to page
 };
-=======
-// function to start the map location from google maps api
-// let venueMap; 
-// function init() {
-//   var mapOptions = {                         // set up the map options
-//     center: new google.maps.LatLng("0.0", "0.0"),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP,
-//     zoom: 13
-//   };
-//                                       //Map() draws a map
-//   venueMap = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
-// }
->>>>>>> main
 
 // function loadScript() {
 //   var script = document.createElement('script');                           //Create <script> element
