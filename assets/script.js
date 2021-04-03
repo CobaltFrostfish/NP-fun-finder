b787837cbe40c2378443c8180708d7db6c84ca87
Evan Mora <evanrmora@gmail.com>
2021-04-02 23:17:51 -0600

    finished for now

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,48 @@
 //get the api keys from the websites I want to use
-//api key= AIzaSyDFJN8KsV0vonVDnuMQg39pedzZxhh7rVs
+//api key= AIzaSyAW220mOd1otBnP8lwhTJXTxRzMby1M2PE
 //above is for geolocation api https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
-
+var showEl= document.getElementById('searchBtn');
 
 let map;
 
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'),{
-    center: { lat: -34.397, lng: 150.644},
+    center: { lat:-118.0186111, lng: 44.6969444},
     zoom:8,
   });
 }
 
 // function to start the map location from google maps api
-let venueMap; 
-function init() {
-  var mapOptions = {                        // set up the map options
-    center: new google.maps.LatLng("0.0", "0.0"),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoom: 13
-  };
-                                      //Map() draws a map
-  venueMap = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
+// let venueMap; 
+// function init() {
+//   var mapOptions = {                        // set up the map options
+//     center: new google.maps.LatLng("0.0", "0.0"),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     zoom: 13
+//   };
+//                                       //Map() draws a map
+//   venueMap = new google.maps.Map(document.getElementById('mapDiv'), mapOptions);
+// }
+
+// function loadScript() {
+//   var script = document.createElement('script');                           //Create <script> element
+//   script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyAW220mOd1otBnP8lwhTJXTxRzMby1M2PE&callback=init';
+
+//   var mapDiv = document.getElementById("mapDiv");
+//   mapDiv.appendChild(script); //add element to page
+// };
+
+
+// window.onload = loadScript; //Onload call
+
+showEl.addEventListener('click',function(){
+  showMap();
+})
+
+function showMap(){
+  
+  document.getElementById('map').style.display='block';
 }
-
-function loadScript() {
-  var script = document.createElement('script');                           //Create <script> element
-  script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyDFJN8KsV0vonVDnuMQg39pedzZxhh7rVs&callback=init';
-
-  var mapDiv = document.getElementById("mapDiv");
-  mapDiv.appendChild(script); //add element to page
-};
-
-
-window.onload = loadScript; //Onload call
-
 
 
 //kendra's-work
@@ -81,4 +89,3 @@ $(document).ready(function() {
 });
 
 
- main
